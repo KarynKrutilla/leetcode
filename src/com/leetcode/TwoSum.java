bcec2e7d73c18edb7aae4ec29cd8cb465dd631e4
Karyn <karyn.levy@gmail.com>
2017-06-24 12:19:13 -0700

    Two Sum Solution

---
+++ src/com/leetcode/TwoSum.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/two-sum/#/description
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seenNumbers = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int requiredValue = target - nums[i];
+            Set seenValues = seenNumbers.keySet();
+            if(seenValues.contains(requiredValue)) {
+                int otherIndex = seenNumbers.get(requiredValue);
+                return new int[]{i, otherIndex};
+            }
+            seenNumbers.put(nums[i], i);
+        }
+        return null;
+    }
+}
