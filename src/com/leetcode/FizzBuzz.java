040f89d2782c1c70db5380ebbac7f77c8b0acb1c
Karyn <karyn.levy@gmail.com>
2017-07-08 09:58:07 -0700

    Fizz Buzz solution

---
+++ src/com/leetcode/FizzBuzz.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/fizz-buzz/#/description
+ */
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> returnList = new ArrayList<>();
+        for(int i = 1; i <= n ; i ++) {
+            String s = "";
+            if(i % 3 == 0) {
+                s += "Fizz";
+            }
+            if(i % 5 == 0) {
+                s += "Buzz";
+            }
+            if("".equals(s)) {
+                s = String.valueOf(i);
+            }
+            returnList.add(s);
+        }
+        return returnList;
+    }
+}
