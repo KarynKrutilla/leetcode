1d97976273acb757defd64898a56ce855739c5aa
Karyn <karyn.levy@gmail.com>
2017-07-22 17:08:35 -0700

    Added ListNode and TreeNode to common package

---
+++ src/com/leetcode/common/ListNode.java
@@ -0,0 +1,12 @@
+package com.leetcode.common;
+
+/**
+ * Definition for a singly-linked list
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+}
