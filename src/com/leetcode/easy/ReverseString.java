33fa4ed318cfea44d919373490a4e1b05e0d3687
Karyn <karyn.levy@gmail.com>
2017-08-29 18:11:23 -0700

    Reverse String

--- src/com/leetcode/easy/ReverseString.java
+++ src/com/leetcode/easy/ReverseString.java
@@ -1,7 +1,14 @@
 package com.leetcode.easy;
 
 /**
- * Created by Karyn on 8/29/2017.
+ * https://leetcode.com/problems/reverse-string/description/
  */
 public class ReverseString {
+    public String reverseString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= s.length(); i++) {
+            sb.append(s.charAt(s.length()-i));
+        }
+        return sb.toString();
+    }
 }
